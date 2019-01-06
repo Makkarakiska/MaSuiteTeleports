7e50c520cd1d53e4639ab97dad07a9c33355b777
Masa <masa@simasasolutions.com>
2019-01-06 19:04:23 +0200

    Refactored

--- src/main/java/fi/matiaspaavilainen/masuiteteleports/bukkit/commands/force/Teleport.java
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/bukkit/commands/force/Teleport.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Player;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 public class Teleport implements CommandExecutor {
 
