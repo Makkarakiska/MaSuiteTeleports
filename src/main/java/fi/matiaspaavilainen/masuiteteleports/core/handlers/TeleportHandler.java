60a1f4e06bbff46a35a9d60100686241884369dc
Masa <masa@simasasolutions.com>
2019-01-27 15:48:44 +0200

    Added tptoggle list

--- src/main/java/fi/matiaspaavilainen/masuiteteleports/core/handlers/TeleportHandler.java
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/core/handlers/TeleportHandler.java
@@ -6,15 +6,13 @@ import fi.matiaspaavilainen.masuiteteleports.core.objects.TeleportRequest;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class TeleportHandler {
 
     public static List<TeleportRequest> requests = new ArrayList<>();
     public static HashMap<UUID, Boolean> lock = new HashMap<>();
+    public static HashSet<UUID> toggles = new HashSet<>();
 
     private MaSuiteTeleports plugin;
 
