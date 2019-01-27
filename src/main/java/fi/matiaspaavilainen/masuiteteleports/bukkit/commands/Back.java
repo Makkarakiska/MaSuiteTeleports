b4e23d8a54e45dee43e317a16a49906041889fa7
Masa <masa@simasasolutions.com>
2019-01-27 12:09:50 +0200

    Updated to BukkitPluginChannel

--- src/main/java/fi/matiaspaavilainen/masuiteteleports/bukkit/commands/Back.java
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/bukkit/commands/Back.java
@@ -1,6 +1,6 @@
 package fi.matiaspaavilainen.masuiteteleports.bukkit.commands;
 
-import fi.matiaspaavilainen.masuitecore.core.objects.PluginChannel;
+import fi.matiaspaavilainen.masuitecore.core.channels.BukkitPluginChannel;
 import fi.matiaspaavilainen.masuiteteleports.bukkit.MaSuiteTeleports;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -37,7 +37,7 @@ public class Back implements CommandExecutor {
             plugin.in_command.add(sender);
 
             Player p = (Player) sender;
-            new PluginChannel(plugin, p, new Object[]{"MaSuiteTeleports", "Back", p.getName()}).send();
+            new BukkitPluginChannel(plugin, p, new Object[]{"MaSuiteTeleports", "Back", p.getName()}).send();
             plugin.in_command.remove(sender);
 
         });
