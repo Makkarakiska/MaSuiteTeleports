3d81e72a75b66ee2401475907236a070b2b984c1
Masa <masa@simasasolutions.com>
2019-06-02 13:03:21 +0300

    Changed eject to leaveVehicle

--- src/main/java/fi/matiaspaavilainen/masuiteteleports/bukkit/listeners/TeleportListener.java
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/bukkit/listeners/TeleportListener.java
@@ -39,7 +39,7 @@ public class TeleportListener implements PluginMessageListener {
                         return;
                     }
                     plugin.tpQue.add(p.getUniqueId());
-                    p.eject();
+                    p.leaveVehicle();
                     p.teleport(new Location(p.getWorld(), in.readDouble(), in.readDouble(), in.readDouble()));
                     Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> plugin.tpQue.remove(p.getUniqueId()), 100);
                 }
@@ -49,7 +49,7 @@ public class TeleportListener implements PluginMessageListener {
                         return;
                     }
                     plugin.tpQue.add(p.getUniqueId());
-                    p.eject();
+                    p.leaveVehicle();
                     p.teleport(new Location(Bukkit.getWorld(in.readUTF()), in.readDouble(), in.readDouble(), in.readDouble()));
                     Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> plugin.tpQue.remove(p.getUniqueId()), 100);
                 }
@@ -61,7 +61,7 @@ public class TeleportListener implements PluginMessageListener {
                     }
                     String[] locInfo = in.readUTF().split(":");
                     Location loc = new Location(Bukkit.getWorld(locInfo[0]), Double.parseDouble(locInfo[1]), Double.parseDouble(locInfo[2]), Double.parseDouble(locInfo[3]), Float.parseFloat(locInfo[4]), Float.parseFloat(locInfo[5]));
-                    p.eject();
+                    p.leaveVehicle();
                     p.teleport(loc);
                 }
                 if (method.equals("GetLocation")) {
@@ -86,7 +86,7 @@ public class TeleportListener implements PluginMessageListener {
         Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {
             if (player != null && target != null) {
                 plugin.tpQue.add(player.getUniqueId());
-                player.eject();
+                player.leaveVehicle();
                 player.teleport(target);
             }
         }, 5);
