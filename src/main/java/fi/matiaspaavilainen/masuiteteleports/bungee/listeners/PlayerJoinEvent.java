ee7036c5d54b93d90b15a1dce6f2e711dc610b55
Matias Paavilainen <matias@paavilainen.net>
2020-01-01 18:16:12 +0200

    Rewrote PlayerJoinEvent

--- src/main/java/fi/matiaspaavilainen/masuiteteleports/bungee/listeners/PlayerJoinEvent.java
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/bungee/listeners/PlayerJoinEvent.java
@@ -3,7 +3,7 @@ package fi.matiaspaavilainen.masuiteteleports.bungee.listeners;
 import fi.matiaspaavilainen.masuitecore.bungee.Utils;
 import fi.matiaspaavilainen.masuitecore.core.configuration.BungeeConfiguration;
 import fi.matiaspaavilainen.masuiteteleports.bungee.MaSuiteTeleports;
-import fi.matiaspaavilainen.masuiteteleports.bungee.commands.SpawnCommand;
+import fi.matiaspaavilainen.masuiteteleports.core.objects.SpawnType;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -25,18 +25,18 @@ public class PlayerJoinEvent implements Listener {
         if (config.load("teleports", "settings.yml").getBoolean("enable-first-spawn") && plugin.api.getPlayerService().getPlayer(e.getPlayer().getUniqueId()) == null) {
             plugin.getProxy().getScheduler().schedule(plugin, () -> {
                 if (utils.isOnline(e.getPlayer())) {
-                    new SpawnCommand(plugin).spawn(e.getPlayer(), 1);
+                    plugin.spawnService.teleportToSpawn(e.getPlayer(), SpawnType.FIRST);
                 }
-            }, plugin.config.load("teleports", "settings.yml").getInt("teleport-delay"), TimeUnit.MILLISECONDS);
+            }, plugin.config.load(null, "config.yml").getInt("teleport-delay"), TimeUnit.MILLISECONDS);
             return;
         }
 
         if (config.load("teleports", "settings.yml").getBoolean("spawn-on-join")) {
             plugin.getProxy().getScheduler().schedule(plugin, () -> {
                 if (utils.isOnline(e.getPlayer())) {
-                    new SpawnCommand(plugin).spawn(e.getPlayer(), 0);
+                    plugin.spawnService.teleportToSpawn(e.getPlayer(), SpawnType.DEFAULT);
                 }
-            }, plugin.config.load("teleports", "settings.yml").getInt("teleport-delay"), TimeUnit.MILLISECONDS);
+            }, plugin.config.load(null, "config").getInt("teleportation-delay"), TimeUnit.MILLISECONDS);
         }
     }
 
