047e62a16ca13aab5df4b496e920e6e9003ddd80
Matias Paavilainen <matias@paavilainen.net>
2020-02-07 20:45:21 +0200

    Added TeleportRequest object

---
+++ src/main/java/dev/masa/masuiteteleports/core/objects/TeleportRequest.java
@@ -0,0 +1,39 @@
+package dev.masa.masuiteteleports.core.objects;
+
+import lombok.*;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.scheduler.ScheduledTask;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class TeleportRequest {
+
+    @NonNull
+    private UUID sender;
+    @NonNull
+    private UUID receiver;
+    @NonNull
+    private TeleportRequestType type;
+    private ScheduledTask timer;
+
+    /**
+     * Get the sender as player
+     * @return returns sender as player or null
+     */
+    public ProxiedPlayer getSenderAsPlayer() {
+        return ProxyServer.getInstance().getPlayer(this.sender);
+    }
+
+    /**
+     * Get the receiver as player
+     * @return returns receiver as player or null
+     */
+    public ProxiedPlayer getReceiverAsPlayer() {
+        return ProxyServer.getInstance().getPlayer(this.receiver);
+    }
+}
