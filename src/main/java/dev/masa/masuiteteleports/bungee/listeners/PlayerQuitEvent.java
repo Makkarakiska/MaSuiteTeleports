6cfb44eda3f1118217215e5bfe2e0145990c080b
Matias Paavilainen <matias@paavilainen.net>
2020-02-07 20:56:30 +0200

    Updated to TeleportRequestService

--- src/main/java/dev/masa/masuiteteleports/bungee/listeners/PlayerQuitEvent.java
+++ src/main/java/dev/masa/masuiteteleports/bungee/listeners/PlayerQuitEvent.java
@@ -1,21 +1,28 @@
 package dev.masa.masuiteteleports.bungee.listeners;
 
+import dev.masa.masuiteteleports.bungee.MaSuiteTeleports;
 import dev.masa.masuiteteleports.core.handlers.TeleportHandler;
-import dev.masa.masuiteteleports.core.services.TeleportRequestService;
+import dev.masa.masuiteteleports.core.objects.TeleportRequest;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class PlayerQuitEvent implements Listener {
 
+    private MaSuiteTeleports plugin;
+
+    public PlayerQuitEvent(MaSuiteTeleports plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
-    public void onQuit(PlayerDisconnectEvent e) {
-        TeleportRequestService request = TeleportHandler.getTeleportRequest(e.getPlayer());
+    public void onQuit(PlayerDisconnectEvent event) {
+        TeleportRequest request = plugin.teleportRequestService.getRequest(event.getPlayer().getUniqueId());
         if (request != null) {
-            request.cancel();
+            plugin.teleportRequestService.cancelRequest(request);
         }
 
-        TeleportHandler.toggles.remove(e.getPlayer().getUniqueId());
-        TeleportHandler.lock.remove(e.getPlayer().getUniqueId());
+        TeleportHandler.toggles.remove(event.getPlayer().getUniqueId());
+        TeleportHandler.lock.remove(event.getPlayer().getUniqueId());
     }
 }
