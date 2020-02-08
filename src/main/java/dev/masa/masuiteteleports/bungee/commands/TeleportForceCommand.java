473ff7688df159128ea9beb1ba2e5c57c03edd5d
Matias Paavilainen <matias@paavilainen.net>
2020-02-08 04:23:30 +0200

    Changed to service

--- src/main/java/dev/masa/masuiteteleports/bungee/commands/TeleportForceCommand.java
+++ src/main/java/dev/masa/masuiteteleports/bungee/commands/TeleportForceCommand.java
@@ -1,41 +1,34 @@
 package dev.masa.masuiteteleports.bungee.commands;
 
-import dev.masa.masuitecore.bungee.Utils;
-import dev.masa.masuitecore.bungee.chat.Formator;
 import dev.masa.masuitecore.core.channels.BungeePluginChannel;
-import dev.masa.masuitecore.core.configuration.BungeeConfiguration;
 import dev.masa.masuitecore.core.objects.Location;
 import dev.masa.masuitecore.core.utils.PlayerFinder;
 import dev.masa.masuiteteleports.bungee.MaSuiteTeleports;
-import dev.masa.masuiteteleports.core.handlers.TeleportHandler;
+import dev.masa.masuiteteleports.core.services.PlayerTeleportService;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class TeleportForceCommand {
 
     private MaSuiteTeleports plugin;
-    private TeleportHandler teleportHandler;
+    private PlayerTeleportService teleportService;
 
     public TeleportForceCommand(MaSuiteTeleports plugin) {
         this.plugin = plugin;
-        teleportHandler = new TeleportHandler(plugin);
+        teleportService = plugin.playerTeleportService;
     }
-
-    private Utils utils = new Utils();
-    private BungeeConfiguration config = new BungeeConfiguration();
-    private Formator formator = new Formator();
 
     // Sender to target
     public void tp(ProxiedPlayer sender, String t, boolean bypass) {
         ProxiedPlayer target = new PlayerFinder().get(t);
-        if (utils.isOnline(target, sender)) {
-            if (TeleportHandler.toggles.contains(target.getUniqueId()) && !bypass) {
-                formator.sendMessage(sender, config.load("teleports", "messages.yml").getString("tptoggle.disabled").replace("%player", target.getName()));
+        if (plugin.utils.isOnline(target, sender)) {
+            if (teleportService.toggles.contains(target.getUniqueId()) && !bypass) {
+                plugin.formator.sendMessage(sender, plugin.config.load("teleports", "messages.yml").getString("tptoggle.disabled").replace("%player", target.getName()));
                 return;
             }
             plugin.playerPositionService.requestPosition(sender);
-            teleportHandler.teleport(sender, target);
-            formator.sendMessage(sender, config.load("teleports", "messages.yml")
+            teleportService.teleportPlayerToPlayer(sender, target);
+            plugin.formator.sendMessage(sender, plugin.config.load("teleports", "messages.yml")
                     .getString("receiver.teleported")
                     .replace("%player%", target.getName())
             );
@@ -46,14 +39,14 @@ public class TeleportForceCommand {
     public void tp(ProxiedPlayer sender, String t1, String t2) {
         ProxiedPlayer target1 = new PlayerFinder().get(t1);
         ProxiedPlayer target2 = new PlayerFinder().get(t2);
-        if (utils.isOnline(target1, sender) && utils.isOnline(target2, sender)) {
+        if (plugin.utils.isOnline(target1, sender) && plugin.utils.isOnline(target2, sender)) {
             plugin.playerPositionService.requestPosition(target1);
-            teleportHandler.teleport(target1, target2);
-            formator.sendMessage(target1, config.load("teleports", "messages.yml")
+            teleportService.teleportPlayerToPlayer(target1, target2);
+            plugin.formator.sendMessage(target1, plugin.config.load("teleports", "messages.yml")
                     .getString("receiver.teleported")
                     .replace("%player%", target2.getName())
             );
-            formator.sendMessage(target2, config.load("teleports", "messages.yml")
+            plugin.formator.sendMessage(target2, plugin.config.load("teleports", "messages.yml")
                     .getString("sender.teleported")
                     .replace("%player%", target1.getName())
             );
@@ -63,7 +56,7 @@ public class TeleportForceCommand {
     // TpCommand player to coordinates in the same world in the same world
     public void tp(ProxiedPlayer sender, String t, Double x, Double y, Double z) {
         ProxiedPlayer target = new PlayerFinder().get(t);
-        if (utils.isOnline(target, sender)) {
+        if (plugin.utils.isOnline(target, sender)) {
             plugin.playerPositionService.requestPosition(target);
             new BungeePluginChannel(plugin, sender.getServer().getInfo(), "MaSuiteTeleports", "PlayerToXYZ", sender.getName(), x, y, z).send();
         }
@@ -73,7 +66,7 @@ public class TeleportForceCommand {
     // TpCommand player to specific location in the same server
     public void tp(ProxiedPlayer sender, String t, Location loc) {
         ProxiedPlayer target = new PlayerFinder().get(t);
-        if (utils.isOnline(target, sender)) {
+        if (plugin.utils.isOnline(target, sender)) {
             plugin.playerPositionService.requestPosition(target);
             new BungeePluginChannel(plugin, sender.getServer().getInfo(), "MaSuiteTeleports", "PlayerToLocation", target.getName(), loc.serialize()).send();
         }
@@ -81,14 +74,14 @@ public class TeleportForceCommand {
 
     public void tphere(ProxiedPlayer sender, String t, boolean bypass) {
         ProxiedPlayer target = new PlayerFinder().get(t);
-        if (utils.isOnline(target, sender)) {
-            if (TeleportHandler.toggles.contains(target.getUniqueId()) && !bypass) {
-                formator.sendMessage(sender, config.load("teleports", "messages.yml").getString("tptoggle.disabled").replace("%player", target.getName()));
+        if (plugin.utils.isOnline(target, sender)) {
+            if (teleportService.toggles.contains(target.getUniqueId()) && !bypass) {
+                plugin.formator.sendMessage(sender, plugin.config.load("teleports", "messages.yml").getString("tptoggle.disabled").replace("%player", target.getName()));
                 return;
             }
             plugin.playerPositionService.requestPosition(target);
-            teleportHandler.teleport(target, sender);
-            formator.sendMessage(sender, config.load("teleports", "messages.yml")
+            teleportService.teleportPlayerToPlayer(target, sender);
+            plugin.formator.sendMessage(sender, plugin.config.load("teleports", "messages.yml")
                     .getString("sender.teleported")
                     .replace("%player%", target.getName())
             );
@@ -96,11 +89,11 @@ public class TeleportForceCommand {
     }
 
     public void tpall(ProxiedPlayer target) {
-        if (utils.isOnline(target)) {
+        if (plugin.utils.isOnline(target)) {
             for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
                 plugin.playerPositionService.requestPosition(player);
-                teleportHandler.teleport(player, target);
-                formator.sendMessage(player, config.load("teleports", "messages.yml")
+                teleportService.teleportPlayerToPlayer(player, target);
+                plugin.formator.sendMessage(player, plugin.config.load("teleports", "messages.yml")
                         .getString("receiver.teleported")
                         .replace("%player%", target.getName())
                 );
