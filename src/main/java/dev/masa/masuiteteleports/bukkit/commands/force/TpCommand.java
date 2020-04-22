1fa9c82ba23cdb42f0a8f1de3d98d5c09641fab2
Matias Paavilainen <matias@paavilainen.net>
2020-04-22 20:42:33 +0300

    Fixes #28

--- src/main/java/dev/masa/masuiteteleports/bukkit/commands/force/TpCommand.java
+++ src/main/java/dev/masa/masuiteteleports/bukkit/commands/force/TpCommand.java
@@ -1,16 +1,11 @@
 package dev.masa.masuiteteleports.bukkit.commands.force;
 
 import dev.masa.masuitecore.acf.BaseCommand;
-import dev.masa.masuitecore.acf.annotation.CommandAlias;
-import dev.masa.masuitecore.acf.annotation.CommandCompletion;
-import dev.masa.masuitecore.acf.annotation.CommandPermission;
-import dev.masa.masuitecore.acf.annotation.Default;
+import dev.masa.masuitecore.acf.annotation.*;
 import dev.masa.masuitecore.core.channels.BukkitPluginChannel;
 import dev.masa.masuiteteleports.bukkit.MaSuiteTeleports;
 import org.bukkit.entity.Player;
 
-
-@CommandAlias("tp")
 public class TpCommand extends BaseCommand {
 
     private MaSuiteTeleports plugin;
@@ -22,6 +17,7 @@ public class TpCommand extends BaseCommand {
     @CommandAlias("tp")
     @CommandPermission("masuiteteleports.teleport.force.player")
     @CommandCompletion("@masuite_players")
+    @Description("Teleport to a player")
     public void teleportSenderToPlayer(Player player, String target) {
         new BukkitPluginChannel(plugin, player, "MaSuiteTeleports", "TeleportForceTo", player.getName(), "TeleportSenderToTarget", target, player.hasPermission("masuiteteleports.teleport.toggle.bypass")).send();
     }
@@ -29,9 +25,11 @@ public class TpCommand extends BaseCommand {
     @CommandAlias("tp")
     @CommandPermission("masuiteteleports.teleport.force.player")
     @CommandCompletion("@masuite_players")
+    @Description("Teleport a player to an other player")
     public void teleportPlayerToPlayer(Player player, String fromPlayer, String toPlayer) {
         new BukkitPluginChannel(plugin, player, "MaSuiteTeleports", "TeleportForceTo", player.getName(), "TeleportTargetToTarget", fromPlayer, toPlayer).send();
     }
+
 
     @CommandAlias("tp")
     @CommandPermission("masuiteteleports.teleport.force.coordinates")
--- src/main/java/dev/masa/masuiteteleports/bungee/listeners/PlayerJoinEvent.java
+++ src/main/java/dev/masa/masuiteteleports/bungee/listeners/PlayerJoinEvent.java
@@ -2,7 +2,6 @@ package dev.masa.masuiteteleports.bungee.listeners;
 
 import dev.masa.masuitecore.bungee.Utils;
 import dev.masa.masuitecore.bungee.events.MaSuitePlayerCreationEvent;
-import dev.masa.masuitecore.core.configuration.BungeeConfiguration;
 import dev.masa.masuiteteleports.bungee.MaSuiteTeleports;
 import dev.masa.masuiteteleports.core.objects.SpawnType;
 import net.md_5.bungee.api.event.PostLoginEvent;
@@ -14,7 +13,6 @@ import java.util.concurrent.TimeUnit;
 public class PlayerJoinEvent implements Listener {
 
     private MaSuiteTeleports plugin;
-    private BungeeConfiguration config = new BungeeConfiguration();
     private Utils utils = new Utils();
 
     private boolean firstSpawnEnabled;
@@ -22,13 +20,13 @@ public class PlayerJoinEvent implements Listener {
 
     public PlayerJoinEvent(MaSuiteTeleports plugin) {
         this.plugin = plugin;
-        this.firstSpawnEnabled = config.load("teleports", "settings.yml").getBoolean("enable-first-spawn");
+        this.firstSpawnEnabled = plugin.config.load("teleports", "settings.yml").getBoolean("enable-first-spawn");
         this.teleportationDelay = plugin.config.load(null, "config.yml").getInt("teleportation-delay");
     }
 
     @EventHandler
     public void onJoin(PostLoginEvent e) {
-        if (config.load("teleports", "settings.yml").getBoolean("spawn-on-join")) {
+        if (plugin.config.load("teleports", "settings.yml").getBoolean("spawn-on-join")) {
             plugin.getProxy().getScheduler().schedule(plugin, () -> {
                 if (utils.isOnline(e.getPlayer())) {
                     plugin.getSpawnService().teleportToSpawn(e.getPlayer(), SpawnType.DEFAULT);
